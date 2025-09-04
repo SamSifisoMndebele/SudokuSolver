b87ef9f872bfd93a03a4c283ccaf899632e1f8f5
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-09-04 14:19:26 +0200

    Added junit testing

--- src/Main.kt
+++ src/Main.kt
@@ -114,28 +114,6 @@ fun main() {
     4	3	8	5	2	6	9	1	7
     7	9	6	3	1	8	4	5	2*/
 
-    val medium3 = """
-        8 5 0 0 0 2 4 0 0
-        7 2 0 0 0 0 0 0 9
-        0 0 4 0 0 0 0 0 0 
-        0 0 0 1 0 7 0 0 2
-        3 0 5 0 0 0 9 0 0 
-        0 4 0 0 0 0 0 0 0
-        0 0 0 0 8 0 0 7 0
-        0 1 7 0 0 0 0 0 0 
-        0 0 0 0 3 6 0 4 0
-        """
-    /*Solution:
-    8	5	9	6	1	2	4	3	7
-    7	2	3	8	5	4	1	6	9
-    1	6	4	3	7	9	5	2	8
-    9	8	6	1	4	7	3	5	2
-    3	7	5	2	6	8	9	1	4
-    2	4	1	5	9	3	7	8	6
-    4	3	2	9	8	1	6	7	5
-    6	1	7	4	2	5	8	9	3
-    5	9	8	7	3	6	2	4	1*/
-
     val hard1 = """
         3 0 0 0 8 0 0 0 0 
         0 0 0 7 0 0 0 0 5 
@@ -186,7 +164,6 @@ fun main() {
         easy2.sudokuSolve(false)
         medium1.sudokuSolve(false)
         medium2.sudokuSolve(false)
-        medium3.sudokuSolve(false)
         hard1.sudokuSolve(false)
         hardest.sudokuSolve(false)
     }
